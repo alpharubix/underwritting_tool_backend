cf0eeff1246972d17963091390cbc43a8ef79f46
Ashok <ashokmommyguy@gmail.com>
2026-05-12 13:36:54 +0530

    fixed uvicorn port for deployment

--- main.py
+++ main.py
@@ -58,4 +58,4 @@ app.include_router(bsa_router)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8080))
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
