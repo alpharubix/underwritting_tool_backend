fb79442a5c2b83eead54179c0dc75859e72fc0ab
Ashok <ashokmommyguy@gmail.com>
2026-05-26 10:59:37 +0530

    Implemented webhook endpoit

--- controller/webhook/scoreme_webhook_controller.py
+++ controller/webhook/scoreme_webhook_controller.py
@@ -23,3 +23,22 @@ async def gst_webhook_receiver(input_data:dict,database:AsyncIOMotorDatabase)->J
     except Exception as e:
         print(e)
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail={"message":"Internal Server Error"})
+
+
+
+
+async def itr_webhook_receiver(input_data:dict,database:AsyncIOMotorDatabase)->JSONResponse:
+    try:
+        if not input_data:
+            HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Body should not be empty")
+
+        itr_collection = database["itr_webhook_data"]
+
+        await itr_collection.insert_one(input_data)
+
+        return JSONResponse(status_code=status.HTTP_200_OK,content={"message":"webhook received successfully"})
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail={"message":"Internal Server Error"})
+
--- routes/webhook_router.py
+++ routes/webhook_router.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException
 from fastapi.routing import APIRouter
 from fastapi.requests import Request
 from starlette import status
-from controller.webhook.scoreme_webhook_controller import gst_webhook_receiver
+from controller.webhook.scoreme_webhook_controller import gst_webhook_receiver,itr_webhook_receiver
 
 webhook_router = APIRouter(prefix='/webhook',tags=['Webhook'])
 
@@ -17,3 +17,15 @@ async def webhook_route(request: Request):
         return await gst_webhook_receiver(input_data=request_body,database=mongodb_database)
     except JSONDecodeError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Invalid Json Body")
+
+
+@webhook_router.post('/itr-service')
+async def itr_route(request: Request):
+    try:
+        request_body = await request.json()
+        mongodb_database = request.app.state.mongo_db
+        return await itr_webhook_receiver(input_data=request_body,database=mongodb_database)
+    except JSONDecodeError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid Json Body")
+    except HTTPException as e:
+        raise e
