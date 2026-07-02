07951be176cae408dcf188f768f06fe5463049e1
PrathamPai2004 <prathameshpai.sdmcet26@gmail.com>
2026-07-02 12:03:35 +0530

    API - 4 , seeded bank parameters, updated the eligibility criteria function - version 1

--- controller/banking_score_controller.py
+++ controller/banking_score_controller.py
@@ -7,7 +7,7 @@ async def get_available_banks(
 	#user_id:str|None=None,
 	# validating access_token
 ):
-	collection = request.app.state.mongo_db["bank_parameters"]
+	collection = request.app.state.mongo_db["banks"]
 	print(collection)
 
 	query={}
@@ -16,7 +16,7 @@ async def get_available_banks(
 		{
 			"_id":0,
 			"bank_name": 1,
-			"bank_id": 1,
+			"bank_code": 1,
 	
 		}
 	).to_list(length=None)
@@ -29,13 +29,13 @@ async def get_available_banks(
 
 async def get_bank_parameter_controller(
 	request:Request,
-	bank_id:str|None=None,
+	bank_code:str|None=None,
 	bank_name:str|None=None
 ):
 	collection = request.app.state.mongo_db["bank_parameters"]
 	
-	if not bank_id and not bank_name:
-		return JSONResponse(content={"message":"Missing parameters | Please provide either Bank_id or Bank_name" },status_code=status.HTTP_400_BAD_REQUEST)
+	if not bank_code and not bank_name:
+		return JSONResponse(content={"message":"Missing parameters | Please provide either Bank_code or Bank_name" },status_code=status.HTTP_400_BAD_REQUEST)
 	query = {}
 
 	if bank_id:
@@ -80,5 +80,28 @@ async def analyse_date_range_controller(
 	return {
 		"status": "true", 
 		"message": "Date range fetched successfully", 
+		"data":data
+	}
+
+async def get_customer_profile_controller(request:Request):
+	user_id = request.state.user_id
+	db = request.app.state.mongo_db
+
+	profile = db["users"]
+	query = {}
+	data = await profile.find(
+		{"user_id":user_id},
+		{
+			"_id":0,
+			"email_id":1,
+			"customer_name":1,
+			"company_name":1
+		}
+	)
+	print(data)
+
+	return {
+		"status":"true",
+		"message":"Customer profile fetched successfully",
 		"data":data
 	}
--- routes/bank_scoring_routes.py
+++ routes/bank_scoring_routes.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI,APIRouter,Request
-from controller.banking_score_controller import get_available_banks,get_bank_parameter_controller,analyse_date_range_controller
+from controller.banking_score_controller import get_available_banks,get_bank_parameter_controller,analyse_date_range_controller,get_customer_profile_controller
 from controller.bank_statement_report import get_report_date_range
+from services.eligibility_service import check_eligibility
 bank_scoring_router = APIRouter(prefix="/v1/bank-scoring",tags=["Bank scoring"])
 
 
@@ -16,10 +17,10 @@ async def get_banks(
 @bank_scoring_router.get("/bank-parameters")
 async def get_bank_parameters(
 	request:Request,
-	bank_id:str | None=None,
+	bank_code:str | None=None,
 	bank_name:str|None=None
 ):
-	return await get_bank_parameter_controller(request,bank_id,bank_name)
+	return await get_bank_parameter_controller(request,bank_code,bank_name)
 
 #API 3 - TO GET THE DATE RANGE 
 @bank_scoring_router.get("/analysis-date-range")
@@ -27,3 +28,20 @@ async def analyse_date_range(
 		request:Request
 ):
 	return await get_report_date_range(request.app.state.mongo_db,request.state.user_id)
+
+# API 4 - ELIGIBILITY CHECK FOR BANK SCORING
+
+@bank_scoring_router.post("/check-eligible")
+async def checkElgible(request:Request):
+	#payload = await request.json()
+	#customer_metrics = payload["customer_parameters"]
+	return await check_eligibility(
+        request
+    )
+
+#additional API for getting customer profile related to his bank details and his bank scoring eligibility
+@bank_scoring_router.get("/customer-profile")
+async def get_customer_profile(
+    request:Request
+):
+    return await get_customer_profile_controller(request)
