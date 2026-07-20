1300bf7239b1e837d68faf06aee0afc99aeb26ad
Ashok <ashokmommyguy@gmail.com>
2026-07-20 18:13:16 +0530

    bad merge

--- config/config.py
+++ config/config.py
@@ -4,25 +4,26 @@ from enum import Enum
 
 # ScoreMe API Endpoints
 # We use the Sandbox URL for now. Later you can switch this easily.
-SCOREME_BASE_URL = "https://sm-bsa.scoreme.in"
-SCOREME_UPLOAD_URL = f"{SCOREME_BASE_URL}/bsa/external/uploadbankstatement"
-SCOREME_MERGE_URL= f"{SCOREME_BASE_URL}/bsa/external/mergebankstatement"
+SCOREME_BASE_URL = "https://sm-bsa-sandbox.scoreme.in"
 SCOREME_BANK_NAME_URL = f"{SCOREME_BASE_URL}/bsa/external/getBankNames"
+SCOREME_UPLOAD_URL = f"{SCOREME_BASE_URL}/bsa/internal/uploadbankstatement"
+SCOREME_REPORT_URL = f"{SCOREME_BASE_URL}/bsa/internal/getbsareport"
+SCOREME_MERGE_URL= f"{SCOREME_BASE_URL}/bsa/external/mergebankstatement"
 
 #Gst service api's
-SCOREME_GST_BASE_URL= "https://sm-gst.scoreme.in"
+SCOREME_GST_BASE_URL= "https://sm-gst-sandbox.scoreme.in"
 SCOREME_GST_INFO_URL = f"{SCOREME_GST_BASE_URL}/gst/external/gstinbasicinfo"
 SCORME_GST_USER_NAME_OTP = f"{SCOREME_GST_BASE_URL}/gst/external/gstgenerateotp"
 SCOREME_GST_OTP_AUTHENTICATION = f"{SCOREME_GST_BASE_URL}/gst/external/gstauthentication"
 SCOREME_GST_POST_GSTIN = f"{SCOREME_GST_BASE_URL}/gst/external/postgstreport"
 
 #Itr service api's
-SCORE_ME_ITR_BASE_URL = "https://sm-itr.scoreme.in"
+SCORE_ME_ITR_BASE_URL = "https://sm-itr-sandbox.scoreme.in"
 SCOREME_FILE_ITR_LINK = f"{SCORE_ME_ITR_BASE_URL}/itr/external/fileAutomatedRequestUsingLink"
 SCOREME_ITR_GET_REFERENCE_STATUS = f"{SCORE_ME_ITR_BASE_URL}/itr/external/getItrCredentialSubmissionStatus"
 
 #Kyc service api's
-SCOREME_KYC_BASE_URL = "https://sm-kyc-sync-prod.scoreme.in"
+SCOREME_KYC_BASE_URL = "https://sm-kyc-sync-sandbox.scoreme.in"
 SCOREME_AADHAAR_VERIFICATION_URL=f"{SCOREME_KYC_BASE_URL}/kyc/external/aadhaarOtp"
 SCOREME_AADHAAR_OTP_VERIFICATION = f"{SCOREME_KYC_BASE_URL}/kyc/external/aadhaarDetail"
 SCOREME_GENERATE_DIGI_URL=f"{SCOREME_KYC_BASE_URL}/kyc/external/initiateDigiLocker"
@@ -31,7 +32,7 @@ SCOEME_DIGILOCKER_DOCUMENT_LIST_URL = f"{SCOREME_KYC_BASE_URL}/kyc/external/docu
 SCOREME_DICILOCKER_DOCUMENT_DOWNLOAD_URL = f"{SCOREME_KYC_BASE_URL}/kyc/external/document"
 
 
-SCOREME_CIBIL_BASE_URL = "https://sm-bda.scoreme.in"
+SCOREME_CIBIL_BASE_URL = "https://sm-bda-sandbox.scoreme.in"
 SCOREME_GENERATE_CIBIL_OTP_URL = f"{SCOREME_CIBIL_BASE_URL}/bda/external/retail"
 SCOREME_VALIDATE_CIBIL_OTP_URL = f"{SCOREME_CIBIL_BASE_URL}/bda/external/validateotp"
 SCOREME_RESEND_CIBIL_OTP_URL=  f"{SCOREME_CIBIL_BASE_URL}/bda/external/resendotp"
