c883613f04ed5bf9cd98bdab90fa8bf97cefd825
Ashok <ashokmommyguy@gmail.com>
2026-08-05 16:40:08 +0530

    Implemented add gst endpoint

--- database/databse_config.py
+++ database/databse_config.py
@@ -7,7 +7,7 @@ dotenv.load_dotenv(override=True)
 
 
 async def get_mongo_db():
-    client = AsyncIOMotorClient(os.getenv("MONGO_URI"),serverSelectionTimeoutMS=5000,maxPoolSize=200,minPoolSize=150)
+    client = AsyncIOMotorClient(os.getenv("MONGO_URI"),serverSelectionTimeoutMS=5000,maxPoolSize=10,minPoolSize=10)
     try:
         db = client["underwriting"]
         return db
@@ -15,7 +15,7 @@ async def get_mongo_db():
         raise e
 
 async def get_postgres_conn():
-    conn = await asyncpg.create_pool(dsn=os.getenv("POSTGRES_URI"),max_size=2,min_size=2)
+    conn = await asyncpg.create_pool(dsn=os.getenv("POSTGRES_URI"),max_size=250,min_size=200)
     try:
         return conn
     except Exception as e:
