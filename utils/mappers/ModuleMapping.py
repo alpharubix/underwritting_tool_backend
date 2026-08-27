3595678c9dc405ec2e736e9a914b6cfc011c4467
Ashok <ashokmommyguy@gmail.com>
2026-08-27 12:05:13 +0530

    fixed indentation error in modulemapping

--- utils/mappers/ModuleMapping.py
+++ utils/mappers/ModuleMapping.py
@@ -1,55 +1,19 @@
 from enum import Enum
 
+
 class ModuleMapping(str, Enum):
     bsa = "bsa_merged_bankstatements"
-    gst="gst_analyzed_report"
+    gst = "gst_analyzed_report"
     itr = "itr_analyzed_report"
     cibil = "cibil_report"
 
+
 class ModuleProjection(Enum):
-
-    # bsa_previous = {
-    #     "$project": {
-    #         "_id": 0,
-
-    #         "bsa_from_date": "$from_date",
-    #         "bsa_to_date": "$to_date",
-
-<<<<<<< Updated upstream
-    #         "tenure": {
-    #             "$add": [
-    #                 {
-    #                     "$dateDiff": {
-    #                         "startDate": "$from_date",
-    #                         "endDate": "$to_date",
-    #                         "unit": "month"
-    #                     }
-    #                 },
-    #                 1
-    #             ]
-    #         },
-=======
-            "tenure": {
-                "$add": [
-                    {
-                        "$dateDiff": {
-                            "startDate": "$from_date",
-                            "endDate": "$to_date",
-                            "unit": "month"
-                        }
-                    },
-                ]
-            },
->>>>>>> Stashed changes
-
-    #         "generated_on": "$created_at"
-    #     }
-    # }
 
     gst = {
         "$project": {
             "_id": 0,
-            "reference_id":1,
+            "reference_id": 1,
             "gst_from_date": {
                 "$toString": {
                     "$arrayElemAt": [
@@ -70,20 +34,27 @@ class ModuleProjection(Enum):
         }
     }
 
-
-    bsa={ # done - bsa_merged_bankstatements col -> looks fine
-        "$project":{
-            "_id":0,
-            "bsa_from_date":"$from_date",
-            "bsa_to_date":"$to_date",
-            "last_merged_reference_id":1,
-            "generated_on":"$created_at"
+    bsa = {
+        "$project": {
+            "_id": 0,
+            "bsa_from_date": "$from_date",
+            "bsa_to_date": "$to_date",
+            "tenure": {
+                "$dateDiff": {
+                    "startDate": "$from_date",
+                    "endDate": "$to_date",
+                    "unit": "month"
+                }
+            },
+            "last_merged_reference_id": 1,
+            "generated_on": "$created_at",
         }
     }
+
     itr = {
         "$project": {
             "_id": 0,
-            "reference_id":1,
+            "reference_id": 1,
             "generated_on": "$created_at"
         }
     }
@@ -91,7 +62,7 @@ class ModuleProjection(Enum):
     cibil = {
         "$project": {
             "_id": 0,
-            "reference_id":1,
+            "reference_id": 1,
             "generated_on": "$cibil_pulled_date"
         }
     }
