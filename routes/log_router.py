f36180764eafacbc55d36d0b907531bcbf84e42a
PrathamPai2004 <prathameshpai.sdmcet26@gmail.com>
2026-09-08 10:33:16 +0530

    Log filters applied - works for unauthenticated requests also

--- routes/log_router.py
+++ routes/log_router.py
@@ -1,4 +1,4 @@
-from fastapi import Request,Response,APIRouter
+from fastapi import APIRouter, Query, Request
 from fastapi.responses import JSONResponse
 from controller.log_controller.logs import view_logs
 
@@ -13,5 +13,25 @@ view_log_projection = {
 }
 
 @log_router.get("/view")
-async def view_logs_route(request:Request,page:int=1):
-    return await view_logs(request,page)
+async def view_logs_route(
+    request: Request,
+    page: int = Query(1, ge=1),
+    path: str | None = None,
+    status_filter: str | None = Query(None, alias="status"),
+    service: str | None = None,
+    method: str | None = None,
+    user_id: str | None = None,
+    role: str | None = None,
+    status_code: int | None = None,
+    ):
+    return await view_logs(
+        request=request,
+        page=page,
+        status_filter=status_filter,
+        service=service,
+        method=method,
+        path=path,
+        user_id=user_id,
+        role=role,
+        status_code=status_code,
+    )
